7b31b005e79a4758acda9a14c9b6b7a9e135387f
Bindu Sagar Singanamalla <bindusagar979@gmail.com>
2025-07-09 23:25:05 -0500

    final

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import Portfolio from './components/Portfolio';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 
 function App() {
-  const appRouter = createBrowserRouter([
+  const appRouter = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Portfolio />,
+      },
+    ],
     {
-      path: "/",
-      element: <Portfolio/>
+      basename: "/portfolio", // ✅ This is the key for GitHub Pages
     }
-      
-  ]);
-  return (
-    <RouterProvider router={appRouter}>
-      <Portfolio />
-    </RouterProvider>
   );
-  
+
+  return <RouterProvider router={appRouter} />;
 }
 
 export default App;
